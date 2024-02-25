01f82f0de1282bdc9e9412ebaad62eec3c2ad3eb
Bender-22 <85234598+Bender-22@users.noreply.github.com>
2024-02-25 11:19:34 +0800

    update circulating supply

--- pages/api/circulating-supply.ts
+++ pages/api/circulating-supply.ts
@@ -13,7 +13,12 @@ export default async function handler(
     provider
   );
   const totalSupply = await tokenContract.totalSupply();
-  const treasurySupply = await tokenContract.balanceOf("0x1703ed1bfacc04b7eb654b297aa4e52ebc008722");
-  const circulatingSupply = totalSupply - treasurySupply;
+  const treasuryBalance = await tokenContract.balanceOf("0x1703ed1bFacC04b7eB654b297aA4E52EBC008722");
+  const safeBalance = await tokenContract.balanceOf("0xD452c1321E03c6e34aD8c6F60b694b1E780c4B75");
+  const hedgeyBalance = await tokenContract.balanceOf("0x912F211B5d24B95CAAE5e568D621d9B72D5bccdB");
+  const governBalance = await tokenContract.balanceOf("0x7767a2e18f0C477b20E85Bd5252b3643D273cD25");
+  const nullBalance = await tokenContract.balanceOf("0x000000000000000000000000000000000000dEaD");
+  const disperseBalance = await tokenContract.balanceOf("0xD152f549545093347A162Dce210e7293f1452150");
+  const circulatingSupply = totalSupply - treasuryBalance - safeBalance - hedgeyBalance - governBalance - nullBalance - disperseBalance;
   res.status(200).send(ethers.formatEther(circulatingSupply));
 }
