f0c2b74d019c607d45d00cf1d6b4690754084e2e
amochuko <7204868+amochuko@users.noreply.github.com>
2026-01-29 09:47:21 +0100

    refactored with new provider setup

--- pages/api/total-supply.ts
+++ pages/api/total-supply.ts
@@ -1,17 +1,12 @@
+import { ethers } from "ethers";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { ethers, getDefaultProvider } from "ethers";
-import ElimuToken from '../../abis/ElimuToken.json'
+import { tokenContract } from "../../helpers";
 
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const provider = getDefaultProvider("mainnet");
-  const tokenContract = new ethers.Contract(
-    "0xe29797910D413281d2821D5d9a989262c8121CC2",
-    ElimuToken.abi,
-    provider
-  );
   const totalSupply = await tokenContract.totalSupply();
-  res.status(200).send(ethers.formatEther(totalSupply));
+
+  res.status(200).json({ totalSupply: ethers.formatEther(totalSupply) });
 }
