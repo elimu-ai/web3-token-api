07e23bf8112e2cc851aab5eb8a0e209c44b10fa3
amochuko <7204868+amochuko@users.noreply.github.com>
2026-01-29 09:46:00 +0100

    move tokenContract to helpers

--- helpers.ts
+++ helpers.ts
@@ -1,7 +1,14 @@
 import { ethers } from "ethers";
+import ElimuToken from "./abis/ElimuToken.json";
 import { appConfig } from "./config";
 
 export const provider = new ethers.JsonRpcProvider(
   appConfig.RPC_URL,
   "mainnet",
+);
+
+export const tokenContract = new ethers.Contract(
+  appConfig.CONTRACT_ADDRESS,
+  ElimuToken.abi,
+  provider,
 );
