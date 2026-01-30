b99a80eacb61ccde2b79c9dc03fbf68a544d2342
Bender-22 <85234598+Bender-22@users.noreply.github.com>
2026-01-30 22:33:04 +0100

    test: add functionality on each pull request
    refs #11

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,17 @@ export default function Index() {
   return (
     <ul>
       <li>
-        <Link href="/api/circulating-supply">
+        <Link href="/api/circulating-supply" id="circulatingSupplyLink">
           /api/circulating-supply
         </Link>
       </li>
       <li>
-        <Link href="/api/total-supply">
+        <Link href="/api/total-supply" id="totalSupplyLink">
           /api/total-supply
         </Link>
       </li>
       <li>
-        <Link href="/api/max-supply">
+        <Link href="/api/max-supply" id="maxSupplyLink">
           /api/max-supply
         </Link>
       </li>
