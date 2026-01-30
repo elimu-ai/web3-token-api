1c69e08a27d33da94cf904af0f89c59fafd2e13f
Bender-22 <85234598+Bender-22@users.noreply.github.com>
2026-01-30 22:03:25 +0100

    fix: invalid status code
    fixes #11

--- pages/api/total-supply.ts
+++ pages/api/total-supply.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { ethers, getDefaultProvider } from "ethers";
+import { ethers } from "ethers";
 import ElimuToken from '../../abis/ElimuToken.json'
 
 export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const provider = getDefaultProvider("mainnet");
+  const provider = new ethers.JsonRpcProvider("https://ethereum-rpc.publicnode.com");
   const tokenContract = new ethers.Contract(
     "0xe29797910D413281d2821D5d9a989262c8121CC2",
     ElimuToken.abi,
